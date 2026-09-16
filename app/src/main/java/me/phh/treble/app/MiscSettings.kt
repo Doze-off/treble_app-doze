4fd977283de55df0171a846df65ccc5e1a1af007
Doze-off <viniciussadxxx@gmail.com>
2026-09-15 23:03:56 -0300

    Remove dexoptAggressive key from MiscSettings
    Removed the dexoptAggressive key from MiscSettings.

--- app/src/main/java/me/phh/treble/app/MiscSettings.kt
+++ app/src/main/java/me/phh/treble/app/MiscSettings.kt
@@ -39,7 +39,6 @@ object MiscSettings : Settings {
     val safeMedia = "key_misc_safe_media"
     val trafficFix = "key_misc_traffic_indicator"
     val performanceTweaks = "key_misc_performance_tweaks"
-    val dexoptAggressive = "key_misc_dexopt_aggressive"
 
     override fun enabled(context: Context): Boolean = true
     fun isRoot() = File(Tools.phhsu).exists()
