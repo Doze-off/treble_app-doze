93ea9f4479ea897d60ceae06327f73f8ce88e037
Doze-off <viniciussadxxx@gmail.com>
2026-09-20 02:33:20 -0300

    Reapply "RogEvents: isolate telephony/charging registration failures from each other"
    This reverts commit 518e5d539be4a998ee274d48c2a3d6988265db3c.

--- app/src/main/java/me/phh/treble/app/RogEvents.kt
+++ app/src/main/java/me/phh/treble/app/RogEvents.kt
@@ -206,16 +206,24 @@ object RogEvents : EntryStartup {
         if (!RogSettings.enabled(ctxt)) return
         appContext = ctxt
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
-            val handler = Handler(HandlerThread("RogEventsThread").apply { start() }.looper)
-            val tm = ctxt.getSystemService(TelephonyManager::class.java)
-            tm.registerTelephonyCallback({ r -> handler.post(r) }, telephonyCallback)
+        try {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+                val handler = Handler(HandlerThread("RogEventsThread").apply { start() }.looper)
+                val tm = ctxt.getSystemService(TelephonyManager::class.java)
+                tm.registerTelephonyCallback({ r -> handler.post(r) }, telephonyCallback)
+            }
+        } catch (t: Throwable) {
+            Log.d("PHH", "RogEvents: failed registering telephony callback", t)
         }
 
-        ctxt.registerReceiver(chargingReceiver, IntentFilter().apply {
-            addAction(Intent.ACTION_POWER_CONNECTED)
-            addAction(Intent.ACTION_POWER_DISCONNECTED)
-        })
+        try {
+            ctxt.registerReceiver(chargingReceiver, IntentFilter().apply {
+                addAction(Intent.ACTION_POWER_CONNECTED)
+                addAction(Intent.ACTION_POWER_DISCONNECTED)
+            })
+        } catch (t: Throwable) {
+            Log.d("PHH", "RogEvents: failed registering charging receiver", t)
+        }
 
         Log.d("PHH", "RogEvents started")
     }
